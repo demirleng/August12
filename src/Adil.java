b29c25f78e4da2400c14b129f80d69a932644e21
demirleng <88108454+demirleng@users.noreply.github.com>
2021-08-12 22:38:26 -0400

    Update Adil.java

--- src/Adil.java
+++ src/Adil.java
@@ -1,6 +1,9 @@
 public class Adil {
     public static void main(String[] args) {
         System.out.println("emrah");
+        System.out.println("emrah");
+        System.out.println("emrah");
+        System.out.println("adil");
 
 
 
